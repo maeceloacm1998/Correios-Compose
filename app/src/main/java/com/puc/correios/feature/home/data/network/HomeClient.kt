a54df4b87e0d04885b813ce439d4bce5ab9616b5
marcelochaves <mcmendes@uolinc.com>
2023-11-01 01:03:05 -0300

    feat: delete service to request api correios with retrofit

--- app/src/main/java/com/puc/correios/feature/home/data/network/HomeClient.kt
+++
@@ -1,9 +0,0 @@
-package com.puc.correios.feature.home.data.network
-
-import com.puc.correios.feature.home.data.network.response.ValidateTokenResponse
-import retrofit2.http.POST
-
-interface HomeClient {
-    @POST("autentica")
-    suspend fun fetchValidateToken(): ValidateTokenResponse
-}
--- app/src/main/java/com/puc/correios/feature/home/data/network/HomeService.kt
+++
@@ -1,8 +0,0 @@
-package com.puc.correios.feature.home.data.network
-
-import com.puc.correios.feature.home.data.network.response.ValidateTokenResponse
-import kotlinx.coroutines.flow.Flow
-
-interface HomeService {
-    suspend fun fetchValidateToken(): Flow<ValidateTokenResponse>
-}
