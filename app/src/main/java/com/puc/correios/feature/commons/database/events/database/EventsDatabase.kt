cead598268dad5be40c82f489336bad167468e53
marcelochaves <mcmendes@uolinc.com>
2023-11-21 12:42:40 -0300

    Revert "feat: add service of notification tracker"
    This reverts commit dc9a71c66d139d3e7456de23b798e3ae90e358d8.

--- app/src/main/java/com/puc/correios/feature/commons/database/events/database/EventsDatabase.kt
+++ app/src/main/java/com/puc/correios/feature/commons/database/events/database/EventsDatabase.kt
@@ -2,8 +2,10 @@ package com.puc.correios.feature.commons.database.events.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
-@Database(entities = [EventsEntity::class], version = 1)
+@Database(entities = [EventsEntity::class], version = 2)
+@TypeConverters(EventsEntityTypeConverter::class)
 abstract class EventsDatabase : RoomDatabase() {
     abstract fun eventsDao(): EventsDAO
 }
